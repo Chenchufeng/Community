97b02d11e68148d34c553565e7d680e49ea3c26b
codedrinker <437121948@qq.com>
2021-03-02 00:22:42 +0800

    reate index.html

--- src/main/java/com/example/blogforum/controller/HelloController.java
+++ src/main/java/com/example/blogforum/controller/HelloController.java
@@ -1,15 +1,17 @@
 package com.example.blogforum.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name",defaultValue = "World") String name, Model model){
-        model.addAttribute("name",name);
+    @GetMapping("/")
+    public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "index";
     }
 }
