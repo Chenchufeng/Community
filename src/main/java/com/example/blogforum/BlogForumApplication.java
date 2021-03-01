2526e6a6f0d910a20f75f33d0d057d3690c8c6c6
Chenchufeng <437121948@qq.com>
2021-03-01 21:51:36 +0800

    init report

---
+++ src/main/java/com/example/blogforum/BlogForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.blogforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogForumApplication.class, args);
+    }
+
+}
