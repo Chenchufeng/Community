faa200baee1a50635ed5f83350f6587d1d0f60c0
codedrinker <437121948@qq.com>
2021-03-06 00:16:16 +0800

    extract github config from code to properties

--- src/main/java/com/example/blogforum/controller/AuthorizeController.java
+++ src/main/java/com/example/blogforum/controller/AuthorizeController.java
@@ -4,12 +4,22 @@ import com.example.blogforum.dto.AccessTokenDTO;
 import com.example.blogforum.model.User;
 import com.example.blogforum.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @Autowired
     AccessTokenDTO accessTokenDTO;
     @Autowired
@@ -20,16 +30,17 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state" ) String state){
         //给accessTokenDTO赋值
-        accessTokenDTO.setClient_id("6efa0c9edf5b747a0e654a5b895e0c3fd2cffb6fe448f000e377eac4f249b3da");
-        accessTokenDTO.setClient_Secret("8ae21a5f3b9fad7388f042f6187eae2d1d4d4f2f3e6542ef7304e8cc5c758ef9");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_Secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
 
         String token = githubProvider.getAccessToken(accessTokenDTO);
         user = githubProvider.getUserByToken(token);
         System.out.println("User name: "+user.getName());
         System.out.println("User email: "+user.getEmail());
+
         return "redirect:/";
     }
 }
